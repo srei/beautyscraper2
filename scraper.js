f168c785818c09bc2605647ebd800e3368c4d5f3
srei <steferu@gmail.com>
2016-10-26 14:50:47 -0700

    added style.css and static folder and static ap.use

--- scraper.js
+++ scraper.js
@@ -6,30 +6,26 @@ const request = require('request');
 const Sequelize = require('sequelize');
 const msg = require('./Postgresql-orm.js');
 const ejs = require('ejs');
+const image = require('image-scraper');
 
 
 const scrapeController = {
 
 	getData: (req, res, next) => {
-
 		const url = req.query.url || 'http://www.dior.com/beauty/en_us/fragrance-beauty/makeup/face/foundation/fr-foundation-foundation.html';
+
 		let productCategory = [];
-		// let headerLogoUrl = "";
 		request(url, (error, response, html) => {
 			let $ = cheerio.load(html);
-				$('.category-title').each((i, el) => {
-					productCategory.push( $(el).text());
-				});
-				// headerLogoUrl = $('.header-logo a').text();
-				// #header > div.diorcom-header-bar.js-header-bar > div.header-bar-content.js-header-content > h3 > a
-
+			$('.category-title').each((i, el) => {
+				productCategory.push( $(el).text());
+			});
 			req.prod = productCategory;
-			// req.headerLogo = headerLogoUrl;
 			next();
 		});
 },
+
 	updateTable: (req, res, next) => {
-		// msg.sequelize.sync({ force: true }).then(() => {});
 
 		msg.Company.create ({ companyname: 'Dior', mainurl: 'http://www.dior.com/beauty/en_us/home.html' }).then(function(company){});
 	  msg.Product.create ({ productname: req.prod[0]}).then(function(company){});
@@ -38,14 +34,15 @@ const scrapeController = {
 	  msg.Product.create ({ productname: req.prod[3]}).then(function(company){});
 	  next();
 	},
+
 	queryTable: (req, res, next) => {
-			let first = req.prod[0];
-			let second = req.prod[1];
-			let third = req.prod[2];
-			let fourth = req.prod[3];
+		let first = req.prod[0];
+		let second = req.prod[1];
+		let third = req.prod[2];
+		let fourth = req.prod[3];
 
 		res.render('index.ejs', {firstDiv: first, secondDiv: second, thirdDiv: third, fourthDiv: fourth});
-				// });
-		}
-}; // end of SCRAPECONTROLLER
+	}
+};
+
 module.exports = scrapeController;
--- server.js
+++ server.js
@@ -6,34 +6,15 @@ const scraperController = require('./scraper');
 const path = require('path');
 const ejs = require('ejs');
 
-// app.get('/mock', (req, res) => {
-// 	//creates schema object and saves to database
-// 	console.log("HI");
-// 	data.create({
-// 		title: 'mockMakeup',
-// 		liquid: true,
-// 		price: 5,
-// 	});
-// });
-
-//request("http://www.sitepoint.com").pipe(fs.createWriteStream("jspro.htm"));  ==> wil write the source code into the file - jspro.htm
-
 //allowing cross-origin resource sharing: allows API to respond to requests from anywhere
 app.use(function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
 	next();
 })
 
-
-// app.use('/', express.static(path.join(__dirname + '/html')));
+app.use(express.static(__dirname + '/Static'));
 
 app.get('/', scraperController.getData, scraperController.updateTable, scraperController.queryTable);
-
-// app.get('/displayPage', scraperController.updateTable);
-
-// app.get('/display', )
-
-
 
 app.listen(3000, (err) => {
 	if(err) console.log("error connecting to Port 3000");
